f1a49fcd3df5e76ad3dff57700bfcda80d200403
Sandesh shrestha <sandeshstha519@gmail.com>
2023-09-29 16:15:25 +0545

    f

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
    <main>
-    <h1 className='text-primary-dark-900'>welcom to code blog!</h1>
+    <h1 className='text-primary-900'>welcome to code blog!</h1>
    </main>
    
   )
