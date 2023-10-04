25436e7973daaecfcd7cbdb63b1611347f5f8265
Sandesh shrestha <sandeshstha519@gmail.com>
2023-10-04 19:33:14 +0545

    f

--- components/BlogCardWithImage.tsx
+++ components/BlogCardWithImage.tsx
@@ -1,15 +1,13 @@
-import { IArticle } from '@/types'
-import Link from 'next/link'
-import React from 'react'
-
-
-interface IPropType{
-    article:IArticle
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { IArticle } from '../types';
+interface IPropType {
+    article: IArticle;
 }
-
-const BlogCardWithImage = ({article}:IPropType) => {
-  return (
-    <div className="bg-gradient-to-r from-violet-500 to-violet-900 rounded-md flex items-center h-64">
+const BlogCardWithImage = ({ article }: IPropType) => {
+    return (
+        <div className="bg-gradient-to-r from-violet-500 to-violet-900 rounded-md flex items-center h-64">
             <Link href="#">
                 <span className="text-2xl w-2/3 text-white p-6 font-bold after:content-[''] after:bg-primary after:block after:w-16 after:h-1 after:rounded-md after:mt-2 cursor-pointer">
                     {article.attributes.Title}
@@ -17,7 +15,7 @@ const BlogCardWithImage = ({article}:IPropType) => {
             </Link>
             <img src="/gitbook.svg" width={140} height={140} />
         </div>
-  )
-}
+    );
+};
 
-export default BlogCardWithImage
+export default BlogCardWithImage;
